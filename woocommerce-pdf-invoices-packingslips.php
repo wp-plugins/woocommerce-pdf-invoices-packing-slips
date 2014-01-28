fcd316254d7e285db391eb69f136c2457de63389
pomegranate <pomegranate@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-28 09:08:57 +0000

    Version 1.1.3+
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-pdf-invoices-packing-slips/tags/1.1.3@846764 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- woocommerce-pdf-invoices-packingslips.php
+++ woocommerce-pdf-invoices-packingslips.php
@@ -143,9 +143,15 @@ if ( !class_exists( 'WooCommerce_PDF_Invoices' ) ) {
 					$attachment_width = $attachment[1];
 					$attachment_height = $attachment[2];
 
-					$attachment_path = get_attached_file( $attachment_id );
+					if (apply_filters('wpo_wcpdf_use_path', true)) {
+						$src = get_attached_file( $attachment_id );
+					} else {
+						$src = $attachment_src;
+					}
+
+					$attachment_path = 
 					
-					printf('<img src="%1$s" width="%2$d" height="%3$d" alt="%4$s" />', $attachment_path, $attachment_width, $attachment_height, esc_attr( $company ) );
+					printf('<img src="%1$s" width="%2$d" height="%3$d" alt="%4$s" />', $src, $attachment_width, $attachment_height, esc_attr( $company ) );
 				}
 			}
 		}
