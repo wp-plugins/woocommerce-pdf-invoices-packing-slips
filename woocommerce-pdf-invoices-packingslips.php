cee694e51298fadeaa0c48dc0054ebfd2930e79e
pomegranate <pomegranate@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-29 07:21:00 +0000

    1.5.12 trunk
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-pdf-invoices-packing-slips/trunk@1148763 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- woocommerce-pdf-invoices-packingslips.php
+++ woocommerce-pdf-invoices-packingslips.php
@@ -3,7 +3,7 @@
  * Plugin Name: WooCommerce PDF Invoices & Packing Slips
  * Plugin URI: http://www.wpovernight.com
  * Description: Create, print & email PDF invoices & packing slips for WooCommerce orders.
- * Version: 1.5.11
+ * Version: 1.5.12
  * Author: Ewout Fernhout
  * Author URI: http://www.wpovernight.com
  * License: GPLv2 or later
@@ -33,7 +33,7 @@ if ( !class_exists( 'WooCommerce_PDF_Invoices' ) ) {
 			self::$plugin_basename = plugin_basename(__FILE__);
 			self::$plugin_url = plugin_dir_url(self::$plugin_basename);
 			self::$plugin_path = trailingslashit(dirname(__FILE__));
-			self::$version = '1.5.11';
+			self::$version = '1.5.12';
 			
 			// load the localisation & classes
 			add_action( 'plugins_loaded', array( $this, 'translations' ) ); // or use init?
@@ -757,6 +757,7 @@ if ( !class_exists( 'WooCommerce_PDF_Invoices' ) ) {
 		 */
 		public function get_order_taxes() {
 			$tax_label = __( 'VAT', 'wpo_wcpdf' ); // register alternate label translation
+			$tax_label = __( 'Tax rate', 'wpo_wcpdf' );
 			$tax_rate_ids = $this->export->get_tax_rate_ids();
 			if ($this->export->order->get_taxes()) {
 				foreach ( $this->export->order->get_taxes() as $key => $tax ) {
